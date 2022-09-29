3c4d7496a100e329f28dc4a489bd8ce9d1db00c2
Your Name <dat054760@gmail.com>
2022-09-29 15:26:13 +0700

    up

--- src/main/java/rikkei/academy/loichao/HelloServlet.java
+++ src/main/java/rikkei/academy/loichao/HelloServlet.java
@@ -1,6 +1,7 @@
 package rikkei.academy.loichao;
 
 import java.io.*;
+import java.util.Date;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -13,13 +14,14 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+
     }
 
     public void destroy() {
